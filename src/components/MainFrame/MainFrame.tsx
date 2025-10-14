b1f2f804a0e4b605fc8a00dd3ee5af60a68a85ac
Jaroslav Strouhal <doors32api@gmail.com>
2025-10-14 11:44:57 +0200

    rename Layout to MainFrame

--- src/components/MainFrame/MainFrame.tsx
+++ src/components/MainFrame/MainFrame.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-export const Layout = styled.div`
+export const MainFrame = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
